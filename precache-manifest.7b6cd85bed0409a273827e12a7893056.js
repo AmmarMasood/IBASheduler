0fcec40f1cd76f402ba304b7db3850c1d8f6cafd
AmmarMasood <ammar.masood98@gmail.com>
2019-12-27 20:38:44 +0500

    Updates

---
+++ precache-manifest.7b6cd85bed0409a273827e12a7893056.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6a374744378b2cf55f4f2f072a6f88d",
+    "url": "/IBASheduler/index.html"
+  },
+  {
+    "revision": "23b4e66df9ffb8311c3d",
+    "url": "/IBASheduler/static/css/main.392ef74f.chunk.css"
+  },
+  {
+    "revision": "968bd7352daa7fc9af1b",
+    "url": "/IBASheduler/static/js/2.f33a3383.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/IBASheduler/static/js/2.f33a3383.chunk.js.LICENSE"
+  },
+  {
+    "revision": "23b4e66df9ffb8311c3d",
+    "url": "/IBASheduler/static/js/main.7cface78.chunk.js"
+  },
+  {
+    "revision": "2955ea32d70dcc763aea",
+    "url": "/IBASheduler/static/js/runtime-main.5f72ea23.js"
+  },
+  {
+    "revision": "efe995d15203259a51a5ae321696a5d4",
+    "url": "/IBASheduler/static/media/IBA-33.efe995d1.png"
+  }
+]);
